5c2ce7169fbcd727372ad0e9bb8305d97e635294
Mahmoud Darwish <imahmwd14@gmail.com>
2020-11-20 20:31:48 +0700

    finished polishing the look

--- mazes/Maze.py
+++ mazes/Maze.py
@@ -13,60 +13,85 @@ class Maze(object):
     EMPTY_CELL = 0
     START_CELL = 1
     GOAL_CELL = 2
-    SEARCHED_CELL = 3
-    PATH_CELL = 4
+    WALL_CELL = 3
+    SEARCHED_CELL = 4
+    PATH_CELL = 5
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.clear_grid()
-        self.walls = self.__construct_walls__()
+        self.width = width if width % 2 == 1 else width + 1
+        self.height = height if height % 2 == 1 else height + 1
+        # self.walls = self.__construct_walls__()
         self.start = ()
         self.goal = ()
         self.start_picked = False
         self.visited = self.__visited_mappings__()
         self.grid = self.__initiate_grid__()
+        self.clear_grid()
 
-    def __initiate_grid__(self):
-        return [[0 for _j in range(self.width)] for _i in range(self.height)]
+    def __initiate_grid__(self, with_walls=True):
+        if with_walls:
+            return [[Maze.WALL_CELL
+                     if
+                     _j == 0 or
+                     _i == 0 or
+                     _j == self.width - 1 or
+                     _i == self.height - 1 or
+                     _i % 2 == 0 or
+                     _j % 2 == 0
+                     else Maze.EMPTY_CELL
+                     for _j in range(self.width)] for _i in range(self.height)]
+        else:
+            return [[Maze.EMPTY_CELL
+                     if
+                     _j == 0 or
+                     _i == 0 or
+                     _j == self.width - 1 or
+                     _i == self.height - 1 or
+                     _i % 2 == 0 or
+                     _j % 2 == 0
+                     else Maze.EMPTY_CELL
+                     for _j in range(self.width)] for _i in range(self.height)]
 
     def __visited_mappings__(self):
         return {i: {j: False for j in range(self.width)} for i in range(self.height)}
 
-    def __construct_walls__(self, with_walls=True):
-        return {i: {j: {d: with_walls
-                        for d in Maze.cardinal_directions
-                        }
-                    for j in range(self.width)
-                    }
-                for i in range(self.height)
-                }
+    # def __construct_walls__(self, with_walls=True):
+    #     return {i: {j: {d: with_walls
+    #                     for d in Maze.cardinal_directions
+    #                     }
+    #                 for j in range(self.width)
+    #                 }
+    #             for i in range(self.height)
+    #             }
 
     def mark_all_cells_not_visited(self):
         self.visited = self.__visited_mappings__()
 
     def reconstruct_walls_and_clear_grid(self, without_walls=False):
-        self.clear_grid()
-        self.walls = self.__construct_walls__(not without_walls)
+        self.grid = self.__initiate_grid__()
+        # self.walls = self.__construct_walls__(not without_walls)
         self.visited = self.__visited_mappings__()
 
     def clear_grid(self):
-        self.grid = self.__initiate_grid__()
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                self.grid[i][j] = Maze.EMPTY_CELL if self.grid[i][j] != Maze.WALL_CELL else self.grid[i][j]
 
     def clean_grid_mark_all_cells_not_visited(self):
-        self.clear_grid()
+        self.grid = self.__initiate_grid__()
         self.mark_all_cells_not_visited()
 
     def clear_all_walls(self, without_walls=False):
-        self.walls = self.__construct_walls__(without_walls)
+        # self.walls = self.__construct_walls__(without_walls)
+        pass
 
     def get_cell_neighbours(self, i, j, with_walls=True):
         if with_walls:
             neighbors = [
-                (i, j + 1, self.walls[i][j][Maze.EAST]),
-                (i, j - 1, self.walls[i][j][Maze.WEST]),
-                (i - 1, j, self.walls[i][j][Maze.NORTH]),
-                (i + 1, j, self.walls[i][j][Maze.SOUTH])
+                (i, j + 2, self.grid[i][j + 1] == Maze.WALL_CELL),
+                (i, j - 2, self.grid[i][j - 1] == Maze.WALL_CELL),
+                (i - 2, j, self.grid[i - 1][j] == Maze.WALL_CELL),
+                (i + 2, j, self.grid[i + 1][j] == Maze.WALL_CELL)
             ]
         else:
             neighbors = [
@@ -85,7 +110,8 @@ class Maze(object):
         return self.visited[i][j]
 
     def is_valid_cell(self, i, j):
-        return i in range(self.height) and j in range(self.width)
+        return i in range(self.height) and j in range(self.width) and not (
+                i == 0 or j == 0 or j == self.width - 1 or i == self.height - 1)
 
     def remove_the_wall_between_cells(self, i, j, neighbour_direction):
         neighbour_i, neighbour_j = -1, -1
@@ -99,14 +125,29 @@ class Maze(object):
         elif neighbour_direction == Maze.WEST:
             neighbour_i, neighbour_j = (i, j - 1)
 
-        if self.is_valid_cell(neighbour_i, neighbour_j):
-            self.walls[i][j][neighbour_direction] = False
-            self.walls[neighbour_i][neighbour_j][Maze.directions_inverses[neighbour_direction]] = False
-        pass
+        if self.is_a_wall_cell(neighbour_i, neighbour_j):
+            self.grid[neighbour_i][neighbour_j] = Maze.EMPTY_CELL
+            pass
 
     @staticmethod
     def get_cell_neighbours_and_directions(i, j):
-        return [(i, j + 1, Maze.EAST), (i, j - 1, Maze.WEST), (i + 1, j, Maze.SOUTH), (i - 1, j, Maze.NORTH)]
+        return [(i, j + 2, Maze.EAST), (i, j - 2, Maze.WEST), (i + 2, j, Maze.SOUTH), (i - 2, j, Maze.NORTH)]
 
     def distance_to_goal(self, i, j):
         return sqrt(abs(self.goal[0] - i) ** 2 + abs(self.goal[1] - j) ** 2)
+
+    def is_a_wall_cell(self, neighbour_i, neighbour_j):
+        return neighbour_i % 2 == 0 or neighbour_j % 2 == 0
+        pass
+
+    def get_cell_search_neighbours(self, i, j):
+        return [
+            (i, j + 1, self.grid[i][j + 1] == Maze.WALL_CELL),
+            (i, j - 1, self.grid[i][j - 1] == Maze.WALL_CELL),
+            (i - 1, j, self.grid[i - 1][j] == Maze.WALL_CELL),
+            (i + 1, j, self.grid[i + 1][j] == Maze.WALL_CELL)
+        ]
+
+    def drop_all_walls(self):
+        self.grid = self.__initiate_grid__(False)
+        pass
